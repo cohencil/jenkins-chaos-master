4925b8c1a20b4b85783d137ea2f655bb94c9aafb
Chen Cohen <cohencil@gmail.com>
2019-07-04 13:18:26 +0300

    replace repo protocol

--- terraform/cloudinit/git.sh
+++ terraform/cloudinit/git.sh
@@ -4,6 +4,6 @@
 #su - ${user} -c "git config --global credential.helper '!aws codecommit credential-helper \$@'"
 su - ${user} -c "git config --global user.name jenkins-chaos-master"
 su - ${user} -c "git config --global user.email jenkins-chaos-master@tikalk.com"
-su - ${user} -c "git clone git@github.com:cohencil/jenkins-chaos-master.git"
-#su - ${user} -c "cd ~/jenkins-chaos-master/jenkins; mkdir jenkins_home"
+su - ${user} -c "git clone https://github.com/cohencil/jenkins-chaos-master.git"
+su - ${user} -c "cd ~/jenkins-chaos-master/jenkins; mkdir jenkins_home"
 #su - ${user} -c "cd ~/jenkins-chaos-master/jenkins; /usr/local/bin/docker-compose down --remove-orphans ;/usr/local/bin/docker-compose up -d"
