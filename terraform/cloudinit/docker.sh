b0d853cf7cdb2cb0c95f77cfa90bdcb2ae52442e
Chen Cohen <cohencil@gmail.com>
2019-07-04 17:57:06 +0300

    change docker.sock permissions

--- terraform/cloudinit/docker.sh
+++ terraform/cloudinit/docker.sh
@@ -6,3 +6,4 @@ service docker start
 usermod -a -G docker ${user}
 curl -L https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+chomd o+rw /var/run/docker.sock
