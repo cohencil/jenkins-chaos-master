e84e3f0bbf38780fe7e8eea2be6a9049df68b969
Chen Cohen <cohencil@gmail.com>
2019-07-05 15:43:48 +0300

    Revert "fix typo"
    This reverts commit fe566357d4856a53377bc68e91a9835b67f7f43e.

--- terraform/cloudinit/docker.sh
+++ terraform/cloudinit/docker.sh
@@ -6,4 +6,4 @@ service docker start
 usermod -a -G docker ${user}
 curl -L https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-chmod o+rw /var/run/docker.sock
+chomd o+rw /var/run/docker.sock
