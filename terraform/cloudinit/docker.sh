6b6d024470a85cc47ceac654f3d63d67e1f2f963
Chen Cohen <cohencil@gmail.com>
2019-07-05 15:43:55 +0300

    Revert "change docker.sock permissions"
    This reverts commit b0d853cf7cdb2cb0c95f77cfa90bdcb2ae52442e.

--- terraform/cloudinit/docker.sh
+++ terraform/cloudinit/docker.sh
@@ -6,4 +6,3 @@ service docker start
 usermod -a -G docker ${user}
 curl -L https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-chomd o+rw /var/run/docker.sock
