1ab2641249b85711afb68b8139ab713caf616e53
Haggai Philip Zagury <hagzag@tikalk.com>
2019-07-06 20:24:35 +0300

    Fix typos

--- terraform/cloudinit/run.sh
+++ terraform/cloudinit/run.sh
@@ -3,7 +3,7 @@
 su - ${user} -c "export AWS_ACCESS_KEY_ID=\`aws configure get aws_access_key_id\`; \
                  export AWS_SECRET_ACCESS_KEY=\`aws configure get aws_secret_access_key\`; \
                  export GIT_PRIVATE_KEY=\`cat ~/.ssh/id_rsa\`;
-                 export GITHUB_CLIENT_SECRET=\`cat ~/.github/secret`; 
+                 export GITHUB_CLIENT_SECRET=\`cat ~/.github/secret\`; 
                  export JENKINS_ADMIN_PASSWORD=\`aws ssm get-parameter --name /JENKINS_CHAOS_MASTER/JENKINS_ADMIN_PASSWORD --with-decryption --query "Parameter.Value" --output text\`;
                  cd ~/jenkins-chaos-master/jenkins; 
                  /usr/local/bin/docker-compose down --remove-orphans ;
