eda7ec9fb3122004a9abce20605aed44d8ea5770
Haggai Philip Zagury <hagzag@tikalk.com>
2019-07-06 14:57:55 +0300

    Update jenkins config + seed job + Change dns name

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -5,7 +5,7 @@ data "aws_route53_zone" "primary" {
 resource "aws_route53_record" "chaos-jenkins" {
 
   zone_id = data.aws_route53_zone.primary.zone_id
-  name    = "chaos-jenkins"
+  name    = "chaos-fuse-ci"
   type    = "A"
   ttl     = "300"
   records = ["${data.aws_eip.eip.public_ip}"]
