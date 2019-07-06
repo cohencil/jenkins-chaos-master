53879b1cdad25934fb4b82fb5ed4778c1f494e9e
Haggai Philip Zagury <hagzag@tikalk.com>
2019-07-06 20:45:57 +0300

    Try fix race condition

--- terraform/cloudinit/aws.sh
+++ terraform/cloudinit/aws.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 su - ${user} -c "aws configure set region ${region}"
-su - ${user} -c "aws ssm get-parameter --name /JENKINS_CHAOS_MASTER/PRIMARY_KEY --with-decryption --query "Parameter.Value" --output text > ~/.ssh/id_rsa"
-su - ${user} -c "mkdir ~/.github"
-su - ${user} -c "aws ssm get-parameter --name /JENKINS_CHAOS_MASTER/GITHUB_CLIENT_SERCRET --with-decryption --query "Parameter.Value" --output text > ~/.github/secret"
-su - ${user} -c "chmod 600 ~/.ssh/id_rsa"
-su - ${user} -c "chmod 600 ~/.github/secret"
+su - ${user} -c "aws ssm get-parameter --name /JENKINS_CHAOS_MASTER/PRIMARY_KEY --with-decryption --query "Parameter.Value" --output text > ~${user}/.ssh/id_rsa"
+su - ${user} -c "mkdir ~${user}/.github"
+su - ${user} -c "aws ssm get-parameter --name /JENKINS_CHAOS_MASTER/GITHUB_CLIENT_SERCRET --with-decryption --query "Parameter.Value" --output text > ~${user}/.github/secret"
+su - ${user} -c "chmod 600 ~${user}/.ssh/id_rsa"
+su - ${user} -c "chmod 600 ~${user}/.github/secret"
